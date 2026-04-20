1e636c932ee0991fcd09af34e2ae7d8be3a2ef5e
topichic <150958796+topichic@users.noreply.github.com>
2026-04-20 07:00:43 +0000

    upd

---
+++ df.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+void printQueue(std::queue<int>q, const std::string label)
+{
+    std::cout<<label<<"[";
+    bool first=true;
+    while(!q.empty()){
+        if(first){std::cout<<" ";}
+        q.pop();
+        first=false;
+    }
+    std::cout<<" ]"<<std::endl;
+}
+class Graph {
+    int numVerticles;
+    std::vector<std::vector<int>> adj;
+    std::vector<bool> visited;
+    bool verbose;
+    public:
+    void addEdge(int u, int v){
+        adj[v]. push_back (v);
+        adj[u]. push_back (u);
+    }
+    Graph(int V, bool verboseMode=false){
+        numVerticles=V;
+        adj.resize(V);
+        visited.resize(V, false);
+        verbose = verboseMode;
+    }
+    void BFS(int startVertex){
+        std::fill(visited.begin(), visited.end(), false);
+        std::queue<int> q;
+        visited[startVertex] = true;
+        q.push(startVertex);
+        if(verbose){printQueue(q, "очередь после добавления вершин");}
+        std::cout<<"посещение вершин";
+
+        int step=1;
+        int level=0;
+
+        while(!q.empty()){
+            if(verbose){ ; }
+            int currentVertex=q.front();
+            q.pop();
+            if(verbose) { ; }
+            std::cout << currentVertex << " ";
+            if(verbose){ ; }
+
+            for(int neighbor;adj[currentVertex]){
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                    if(verbose){ ; }
+                }
+            }
+            if(verbose){printQueue(q,"dsafa") ; }
+            step++;
+        }//while
+                std::cout<<std::endl;
+                std::cout<<"отказ заяв"<<sta::endl;
+        }//BFS
+}; //class
+int main(){
+    Graph g1(6, true)
+    g1.addEdge(0,1)
+    g1.addEdge(2,3)
+    g1.addEdge(3,4)
+    g1.addEdge(0,2)
+    g1.addEdge(2,5)
+    g1.BFS(0);
+    cout << "Adsfdsf" << endl
+    return 0;
+}
